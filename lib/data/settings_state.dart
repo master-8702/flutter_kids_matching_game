8f564a7bfffe89c59052632557cb706d2beda6f6
Ibrahim Selman <ibroselman@gmail.com>
2023-11-01 21:20:58 +0300

    Update settings state file

--- lib/data/settings_state.dart
+++ lib/data/settings_state.dart
@@ -1,26 +1,23 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/widgets.dart';
 
 class SettingState {
   final Locale locale;
   final Color themeColor;
-  final String themeColorString;
   final String level;
   final String selectedLanguage;
   final int themeColorCode;
 
-  SettingState(
-      {required this.locale,
-      required this.themeColor,
-      required this.level,
-      required this.selectedLanguage,
-      required this.themeColorCode,
-      required this.themeColorString});
+  SettingState({
+    required this.locale,
+    required this.themeColor,
+    required this.level,
+    required this.selectedLanguage,
+    required this.themeColorCode,
+  });
 
   SettingState copyWith({
     Locale? locale,
     Color? themeColor,
-    String? themeColorString,
     String? level,
     String? selectedLanguage,
     int? themeColorCode,
@@ -28,7 +25,6 @@ class SettingState {
     return SettingState(
       locale: locale ?? this.locale,
       themeColor: themeColor ?? this.themeColor,
-      themeColorString: themeColorString ?? this.themeColorString,
       level: level ?? this.level,
       selectedLanguage: selectedLanguage ?? this.selectedLanguage,
       themeColorCode: themeColorCode ?? this.themeColorCode,
@@ -39,9 +35,8 @@ class SettingState {
   bool operator ==(covariant SettingState other) {
     if (identical(this, other)) return true;
 
-    return other.locale == locale &&
+    return other.locale == locale && 
         other.themeColor == themeColor &&
-        other.themeColorString == themeColorString &&
         other.level == level &&
         other.selectedLanguage == selectedLanguage &&
         other.themeColorCode == themeColorCode;
@@ -51,7 +46,6 @@ class SettingState {
   int get hashCode {
     return locale.hashCode ^
         themeColor.hashCode ^
-        themeColorString.hashCode ^
         level.hashCode ^
         selectedLanguage.hashCode ^
         themeColorCode.hashCode;
@@ -59,6 +53,6 @@ class SettingState {
 
   @override
   String toString() {
-    return 'SettingState(locale: $locale, themeColor: $themeColor, themeColorString: $themeColorString, level: $level, selectedLanguage: $selectedLanguage, themeColorCode: $themeColorCode)';
+    return 'SettingState(locale: $locale, themeColor: $themeColor, level: $level, selectedLanguage: $selectedLanguage, themeColorCode: $themeColorCode)';
   }
 }
