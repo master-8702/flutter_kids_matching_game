c148aae4c58f88263cd2e29aec05ab37887a0f4a
Ibrahim Selman <ibroselman@gmail.com>
2023-11-03 23:34:23 +0300

    Breakdown fruit game screen into smaller widgets

---
+++ lib/presentation/fruit_game_screen/fruit_game_result.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:flutter_kids_matching_game/utilities/utilities.dart';
+import 'package:flutter_kids_matching_game/presentation/fruit_game_screen_controller.dart';
+
+class FruitGameResult extends ConsumerWidget {
+  const FruitGameResult({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final fruitGameController = ref.watch(fruitGameScreenControllerProvider);
+
+    return Column(
+      children: [
+        const SizedBox(
+          height: 100,
+        ),
+        Text(
+          "${fruitGameController.score}/60",
+          style: const TextStyle(fontSize: 50),
+        ),
+        Text(
+          AppLocalizations.of(context)!.gameOver,
+          style: TextStyle(
+              color:
+                  fruitGameController.score >= 50 ? Colors.green : Colors.red,
+              fontWeight: FontWeight.bold,
+              fontSize: 40),
+        ),
+        Center(
+          child: ElevatedButton(
+            child: Text(
+              AppLocalizations.of(context)!.tryAgain, // style: TextStyle(
+              //   fontSize: 30,
+              // ),
+            ),
+            onPressed: () async {
+              await ref
+                  .read(fruitGameScreenControllerProvider.notifier)
+                  .initGame(context);
+            },
+          ),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        if ((fruitGameController.score >= 50) &&
+            Utilities().getGameLevel() <= 4)
+          Center(
+            child: ElevatedButton(
+              child: Text(
+                AppLocalizations.of(context)!.nextGame,
+                // style: TextStyle(
+                //   fontSize: 30,
+                // ),
+              ),
+              onPressed: () async {
+                Utilities().nextGameLevel();
+                var temp = ref.read(fruitGameScreenControllerProvider.notifier);
+                await temp.initGame(context);
+              },
+            ),
+          )
+      ],
+    );
+  }
+}
---
+++ lib/presentation/fruit_game_screen/fruit_game_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_kids_matching_game/presentation/fruit_game_screen/fruit_choice_a.dart';
+import 'package:flutter_kids_matching_game/presentation/fruit_game_screen/fruit_choice_b.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:flutter_kids_matching_game/presentation/fruit_game_screen_controller.dart';
+import 'package:flutter_kids_matching_game/presentation/color_game_screen/color_choice_a.dart';
+import 'package:flutter_kids_matching_game/presentation/color_game_screen/color_choice_b.dart';
+import 'package:flutter_kids_matching_game/presentation/color_game_screen/color_game_result.dart';
+
+class FruitGameScreen extends ConsumerWidget {
+  FruitGameScreen({Key? key}) : super(key: key);
+
+  // we use it to play sounds
+  final _audioController = AudioPlayer();
+  var isFirstTime = true;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final fruitGameController = ref.watch(fruitGameScreenControllerProvider);
+
+// here we are calling initGame() to initialize choice lists inside a build method
+// but only once, we will not call initGame() on every build method call.
+// The reason is that the app implements localization features and support three
+// languages, so the game choice at a certain point are different based on the
+// selected language. and in order to display those language specific choices we
+// need access to BuildContext, and the only place to get the BuildContext is the
+// build method.
+// I could also use ConsumerStatefulWidget and do that inside the initState(),
+// but i think this one better for memory and performance since its stateless.
+    if (isFirstTime) {
+      ref.read(fruitGameScreenControllerProvider.notifier).initGame(context);
+      isFirstTime = false;
+    }
+
+    final localization = AppLocalizations.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '${localization!.score} :  ${fruitGameController.score}',
+          style: const TextStyle(fontSize: 30),
+        ),
+      ),
+      // floating action button for restarting the game
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await ref
+              .read(fruitGameScreenControllerProvider.notifier)
+              .initGame(context);
+        },
+        child: const Icon(Icons.refresh),
+      ),
+      body: Material(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                if (!fruitGameController.isGameOver)
+                  Row(
+                    children: <Widget>[
+                      // left side choices with names
+                      const FruitChoiceA(),
+                      const Spacer(),
+                      // right side choices with pictures
+                      FruitChoiceB(
+                        audioController: _audioController,
+                      )
+                    ],
+                  ),
+                if (fruitGameController.isGameOver) const ColorGameResult(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
