fbccb83958c960982c32627c01a12b2e137b3a8d
master-8702 <ibroselman@gmail.com>
2025-05-08 22:32:45 +0300

    refactor: extract animal game data from controller to its own file

---
+++ lib/features/game/data/game_data.dart
@@ -0,0 +1,159 @@
+import 'package:flutter_kids_matching_game/features/game/domain/game_item.dart';
+
+/// This class contains the game data for the animals game.
+/// It provides a list of game items with their images, names, values, and levels.
+/// The game items are used in the game to match the images with their names.
+class GameData {
+  static final List<GameItem> animals = [
+    GameItem(
+        image: "assets/images/animals/sheep.png",
+        name: 'sheep',
+        value: 'sheep',
+        level: 1),
+    GameItem(
+        image: "assets/images/animals/lion.png",
+        name: 'lion',
+        value: 'lion',
+        level: 1),
+    GameItem(
+        image: "assets/images/animals/elephant.png",
+        name: 'elephant',
+        value: 'elephant',
+        level: 1),
+    GameItem(
+        image: "assets/images/animals/ostrich.png",
+        name: 'ostrich',
+        value: 'ostrich',
+        level: 1),
+    GameItem(
+        image: "assets/images/animals/giraffe.png",
+        name: 'giraffe',
+        value: 'giraffe',
+        level: 1),
+    GameItem(
+        image: "assets/images/animals/camel.png",
+        name: 'camel',
+        value: 'camel',
+        level: 1),
+    GameItem(
+        image: "assets/images/animals/chicken.png",
+        name: 'chicken',
+        value: 'chicken',
+        level: 2),
+    GameItem(
+        image: "assets/images/animals/leopard.png",
+        name: 'leopard',
+        value: 'leopard',
+        level: 2),
+    GameItem(
+        image: "assets/images/animals/monkey.png",
+        name: 'monkey',
+        value: 'monkey',
+        level: 2),
+    GameItem(
+        image: "assets/images/animals/zebra.png",
+        name: 'zebra',
+        value: 'zebra',
+        level: 2),
+    GameItem(
+        image: "assets/images/animals/owl.png",
+        name: 'owl',
+        value: 'owl',
+        level: 2),
+    GameItem(
+        image: "assets/images/animals/tiger.png",
+        name: 'tiger',
+        value: 'tiger',
+        level: 2),
+    GameItem(
+        image: "assets/images/animals/ox.png",
+        name: 'ox',
+        value: 'ox',
+        level: 3),
+    GameItem(
+        image: "assets/images/animals/squirrel.png",
+        name: 'squirrel',
+        value: 'squirrel',
+        level: 3),
+    GameItem(
+        image: "assets/images/animals/mouse.png",
+        name: 'mouse',
+        value: 'mouse',
+        level: 3),
+    GameItem(
+        image: "assets/images/animals/gorilla.png",
+        name: 'gorilla',
+        value: 'gorilla',
+        level: 3),
+    GameItem(
+        image: "assets/images/animals/chimpanzee.png",
+        name: 'chimpanzee',
+        value: 'chimpanzee',
+        level: 3),
+    GameItem(
+        image: "assets/images/animals/buffalo.png",
+        name: 'buffalo',
+        value: 'buffalo',
+        level: 3),
+    GameItem(
+        image: "assets/images/animals/cheetah.png",
+        name: 'cheetah',
+        value: 'cheetah',
+        level: 4),
+    GameItem(
+        image: "assets/images/animals/crocodile.png",
+        name: 'crocodile',
+        value: 'crocodile',
+        level: 4),
+    GameItem(
+        image: "assets/images/animals/duck.png",
+        name: 'duck',
+        value: 'duck',
+        level: 4),
+    GameItem(
+        image: "assets/images/animals/fox.png",
+        name: 'fox',
+        value: 'fox',
+        level: 4),
+    GameItem(
+        image: "assets/images/animals/hippopotamus.png",
+        name: 'hippopotamus',
+        value: 'hippopotamus',
+        level: 4),
+    GameItem(
+        image: "assets/images/animals/mule.png",
+        name: 'mule',
+        value: 'mule',
+        level: 4),
+    GameItem(
+        image: "assets/images/animals/horse.png",
+        name: 'horse',
+        value: 'horse',
+        level: 5),
+    GameItem(
+        image: "assets/images/animals/kangaroo.png",
+        name: 'kangaroo',
+        value: 'kangaroo',
+        level: 5),
+    GameItem(
+        image: "assets/images/animals/deer.png",
+        name: 'deer',
+        value: 'deer',
+        level: 5),
+    GameItem(
+        image: "assets/images/animals/panther.png",
+        name: 'panther',
+        value: 'panther',
+        level: 5),
+    GameItem(
+        image: "assets/images/animals/penguin.png",
+        name: 'penguin',
+        value: 'penguin',
+        level: 5),
+    GameItem(
+        image: "assets/images/animals/rhinoceros.png",
+        name: 'rhinoceros',
+        value: 'rhinoceros',
+        level: 5),
+  ];
+}
