4411af1a35c66a993008f591e3dc7aa567249657
master-8702 <ibroselman@gmail.com>
2025-05-08 17:38:27 +0300

    feat: update the whole settings screen UI with new look

---
+++ lib/features/settings/presentation/screens/language_selector.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+import 'package:flutter_kids_matching_game/core/constants/setting_choices.dart';
+import 'package:flutter_kids_matching_game/features/settings/presentation/screens/build_dropdown.dart';
+import 'package:flutter_kids_matching_game/features/settings/presentation/notifiers/settings_notifier.dart';
+
+/// A widget that allows the user to select a language for the app.
+class LanguageSelector extends ConsumerWidget {
+  const LanguageSelector({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final settings = ref.watch(settingsNotifierProvider);
+
+    return buildDropdown<AppLanguage>(
+      context: context,
+      label: 'app_language'.tr(),
+      value: settings.selectedLanguage,
+      items: AppLanguage.values.map((lang) {
+        return DropdownMenuItem<AppLanguage>(
+          value: lang,
+          child: Text(
+            lang.translatedName,
+            style: Theme.of(context).textTheme.labelMedium,
+          ),
+        );
+      }).toList(),
+      onChanged: (appLanguage) {
+        if (appLanguage != null) {
+          // updating the new selected language on the UI
+          context.setLocale(appLanguage.locale);
+          // updating the state and local storage with the new selected language
+          ref.read(settingsNotifierProvider.notifier).setLanguage(appLanguage);
+        }
+      },
+    );
+  }
+}
