8fbb1dd4142cdb32157885d64eb5cd80d335d017
master-8702 <ibroselman@gmail.com>
2025-05-11 23:55:09 +0300

    feat: update gameData file to include fruit and color data

--- lib/features/game/data/game_data.dart
+++ lib/features/game/data/game_data.dart
@@ -156,4 +156,310 @@ class GameData {
         value: 'rhinoceros',
         level: 5),
   ];
+
+  static final List<GameItem> colors = [
+    GameItem(
+        image: "assets/images/colors/White.png",
+        name: 'white',
+        value: 'white',
+        level: 1),
+    GameItem(
+        image: "assets/images/colors/Black.png",
+        name: 'black',
+        value: 'black',
+        level: 1),
+    GameItem(
+        image: "assets/images/colors/Blue.png",
+        name: 'blue',
+        value: 'blue',
+        level: 1),
+    GameItem(
+        image: "assets/images/colors/Red.png",
+        name: 'red',
+        value: 'red',
+        level: 1),
+    GameItem(
+        image: "assets/images/colors/Green.png",
+        name: 'green',
+        value: 'green',
+        level: 1),
+    GameItem(
+        image: "assets/images/colors/Yellow.png",
+        name: 'yellow',
+        value: 'yellow',
+        level: 1),
+    GameItem(
+        image: "assets/images/colors/Orange.png",
+        name: 'orange_color',
+        value: 'orange_color',
+        level: 2),
+    GameItem(
+        image: "assets/images/colors/Pink.png",
+        name: 'pink',
+        value: 'pink',
+        level: 2),
+    GameItem(
+        image: "assets/images/colors/Purple.png",
+        name: 'purple',
+        value: 'purple',
+        level: 2),
+    GameItem(
+        image: "assets/images/colors/Cyan.png",
+        name: 'cyan',
+        value: 'cyan',
+        level: 2),
+    GameItem(
+        image: "assets/images/colors/Mustard.png",
+        name: 'mustard',
+        value: 'mustard',
+        level: 2),
+    GameItem(
+        image: "assets/images/colors/Gray.png",
+        name: 'gray',
+        value: 'gray',
+        level: 2),
+    GameItem(
+        image: "assets/images/colors/Navy Blue.png",
+        name: 'navyblue',
+        value: 'navyblue',
+        level: 3),
+    GameItem(
+        image: "assets/images/colors/Golden.png",
+        name: 'golden',
+        value: 'golden',
+        level: 3),
+    GameItem(
+        image: "assets/images/colors/Burgundy.png",
+        name: 'burgundy',
+        value: 'burgundy',
+        level: 3),
+    GameItem(
+        image: "assets/images/colors/Lime.png",
+        name: 'lime',
+        value: 'lime',
+        level: 3),
+    GameItem(
+        image: "assets/images/colors/Cream.png",
+        name: 'cream',
+        value: 'cream',
+        level: 3),
+    GameItem(
+        image: "assets/images/colors/Aqua.png",
+        name: 'aqua',
+        value: 'aqua',
+        level: 3),
+    GameItem(
+        image: "assets/images/colors/Beige.png",
+        name: 'beige',
+        value: 'beige',
+        level: 4),
+    GameItem(
+        image: "assets/images/colors/Silver.png",
+        name: 'silver',
+        value: 'silver',
+        level: 4),
+    GameItem(
+        image: "assets/images/colors/Olive.png",
+        name: 'olive',
+        value: 'olive',
+        level: 4),
+    GameItem(
+        image: "assets/images/colors/Coral.png",
+        name: 'coral',
+        value: 'coral',
+        level: 4),
+    GameItem(
+        image: "assets/images/colors/Mauve.png",
+        name: 'mauve',
+        value: 'mauve',
+        level: 4),
+    GameItem(
+        image: "assets/images/colors/Teal.png",
+        name: 'teal',
+        value: 'teal',
+        level: 4),
+    GameItem(
+        image: "assets/images/colors/Bronze.png",
+        name: 'bronze',
+        value: 'bronze',
+        level: 5),
+    GameItem(
+        image: "assets/images/colors/Maroon.png",
+        name: 'maroon',
+        value: 'maroon',
+        level: 5),
+    GameItem(
+        image: "assets/images/colors/Lavender.png",
+        name: 'lavender',
+        value: 'lavender',
+        level: 5),
+    GameItem(
+        image: "assets/images/colors/Rust.png",
+        name: 'rust',
+        value: 'rust',
+        level: 5),
+    GameItem(
+        image: "assets/images/colors/Peach.png",
+        name: 'peach_color',
+        value: 'peach_color',
+        level: 5),
+    GameItem(
+        image: "assets/images/colors/Magenta.png",
+        name: 'magenta',
+        value: 'magenta',
+        level: 5),
+  ];
+
+  static final List<GameItem> fruits = [
+    GameItem(
+        image: "assets/images/fruits/apple.png",
+        name: 'apple',
+        value: 'apple',
+        level: 1),
+    GameItem(
+        image: "assets/images/fruits/banana.png",
+        name: 'banana',
+        value: 'banana',
+        level: 1),
+    GameItem(
+        image: "assets/images/fruits/strawberry.png",
+        name: 'strawberry',
+        value: 'strawberry',
+        level: 1),
+    GameItem(
+        image: "assets/images/fruits/pineapple.png",
+        name: 'pineapple',
+        value: 'pineapple',
+        level: 1),
+    GameItem(
+        image: "assets/images/fruits/orange.png",
+        name: 'orange_fruit',
+        value: 'orange_fruit',
+        level: 1),
+    GameItem(
+        image: "assets/images/fruits/grapes.png",
+        name: 'grapes',
+        value: 'grapes',
+        level: 1),
+    GameItem(
+        image: "assets/images/fruits/avocado.png",
+        name: 'avocado',
+        value: 'avocado',
+        level: 2),
+    GameItem(
+        image: "assets/images/fruits/apricot.png",
+        name: 'apricot',
+        value: 'apricot',
+        level: 2),
+    GameItem(
+        image: "assets/images/fruits/blueberry.png",
+        name: 'blueberry',
+        value: 'blueberry',
+        level: 2),
+    GameItem(
+        image: "assets/images/fruits/mango.png",
+        name: 'mango',
+        value: 'mango',
+        level: 2),
+    GameItem(
+        image: "assets/images/fruits/pear.png",
+        name: 'pear',
+        value: 'pear',
+        level: 2),
+    GameItem(
+        image: "assets/images/fruits/watermelon.png",
+        name: 'watermelon',
+        value: 'watermelon',
+        level: 2),
+    GameItem(
+        image: "assets/images/fruits/cactusfruit.png",
+        name: 'cactusfruit',
+        value: 'cactusfruit',
+        level: 3),
+    GameItem(
+        image: "assets/images/fruits/cherry.png",
+        name: 'cherry',
+        value: 'cherry',
+        level: 3),
+    GameItem(
+        image: "assets/images/fruits/guava.png",
+        name: 'guava',
+        value: 'guava',
+        level: 3),
+    GameItem(
+        image: "assets/images/fruits/lemon.png",
+        name: 'lemon',
+        value: 'lemon',
+        level: 3),
+    GameItem(
+        image: "assets/images/fruits/papaya.png",
+        name: 'papaya',
+        value: 'papaya',
+        level: 3),
+    GameItem(
+        image: "assets/images/fruits/tangerine.png",
+        name: 'tangerine',
+        value: 'tangerine',
+        level: 3),
+    GameItem(
+        image: "assets/images/fruits/coconut.png",
+        name: 'coconut',
+        value: 'coconut',
+        level: 4),
+    GameItem(
+        image: "assets/images/fruits/fig.png",
+        name: 'fig',
+        value: 'fig',
+        level: 4),
+    GameItem(
+        image: "assets/images/fruits/citron.png",
+        name: 'citron',
+        value: 'citron',
+        level: 4),
+    GameItem(
+        image: "assets/images/fruits/plum.png",
+        name: 'plum',
+        value: 'plum',
+        level: 4),
+    GameItem(
+        image: "assets/images/fruits/soursop.png",
+        name: 'soursop',
+        value: 'soursop',
+        level: 4),
+    GameItem(
+        image: "assets/images/fruits/whitesapote.png",
+        name: 'whitesapote',
+        value: 'whitesapote',
+        level: 4),
+    GameItem(
+        image: "assets/images/fruits/date.png",
+        name: 'date',
+        value: 'date',
+        level: 5),
+    GameItem(
+        image: "assets/images/fruits/lychee.png",
+        name: 'lychee',
+        value: 'lychee',
+        level: 5),
+    GameItem(
+        image: "assets/images/fruits/nectarine.png",
+        name: 'nectarine',
+        value: 'nectarine',
+        level: 5),
+    GameItem(
+        image: "assets/images/fruits/passionfruit.png",
+        name: 'passionfruit',
+        value: 'passionfruit',
+        level: 5),
+    GameItem(
+        image: "assets/images/fruits/peach.png",
+        name: 'peach_fruit',
+        value: 'peach_fruit',
+        level: 5),
+    GameItem(
+        image: "assets/images/fruits/pomegranate.png",
+        name: 'pomegranate',
+        value: 'pomegranate',
+        level: 5)
+  ];
 }
