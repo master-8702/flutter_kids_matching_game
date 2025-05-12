3d9a063f4ec006e6c4100bd54335c45add1035d6
master-8702 <ibroselman@gmail.com>
2025-05-12 15:23:10 +0300

    refactor: update the FruitGameScreen with the new common notifier, result screen and widgets

---
+++ lib/features/game/presentation/fruit_game_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+import 'package:flutter_kids_matching_game/core/theme/app_theme.dart';
+import 'package:flutter_kids_matching_game/core/utilities/app_sizes.dart';
+import 'package:flutter_kids_matching_game/core/constants/game_type.dart';
+import 'package:flutter_kids_matching_game/core/constants/setting_choices.dart';
+import 'package:flutter_kids_matching_game/features/game/presentation/game_result.dart';
+import 'package:flutter_kids_matching_game/features/game/presentation/widgets/choice_a_draggable.dart';
+import 'package:flutter_kids_matching_game/features/game/presentation/widgets/choice_b_drag_target.dart';
+import 'package:flutter_kids_matching_game/features/game/presentation/notifiers/game_screen_notifier.dart';
+import 'package:flutter_kids_matching_game/features/settings/presentation/notifiers/settings_notifier.dart';
+
+/// This widget is used to show the fruit game screen, where the user can
+/// drag the left side names of fruits and drop them on right side of the fruit
+/// pictures.
+class FruitGameScreen extends ConsumerStatefulWidget {
+  const FruitGameScreen({Key? key}) : super(key: key);
+  @override
+  FruitGameScreenState createState() => FruitGameScreenState();
+}
+
+class FruitGameScreenState extends ConsumerState<FruitGameScreen> {
+  late final AudioPlayer _audioController;
+
+  // here we are calling initGame() to initialize choice lists based on the
+  // selected game type and level.
+  void _restartGame() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      final level = ref.read(settingsNotifierProvider).selectedLevel;
+
+      ref
+          .read(gameScreenNotifierProvider.notifier)
+          .initGame(currentLevel: level.number, gameType: GameType.fruit);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // here we are initializing the audio controller
+    _audioController = AudioPlayer();
+
+    // here we are calling restartGame(initGame) to initialize choice lists
+    _restartGame();
+  }
+
+  @override
+  void dispose() {
+    // here we are disposing the audio controller
+    _audioController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final fruitGameController = ref.watch(gameScreenNotifierProvider);
+    final isGameOver = fruitGameController.isGameOver;
+    return Container(
+      color: AppTheme.primaryColor.withAlpha(0),
+      child: Scaffold(
+        // backgroundColor: AppTheme.primaryColor.withAlpha(25),
+        appBar: AppBar(
+          title: Text(
+            '${'score'.tr()} :  ${fruitGameController.score}',
+            style: const TextStyle(fontSize: 30),
+          ),
+        ),
+        body: Container(
+            decoration: !isGameOver
+                // when the game screen is shown
+                ? BoxDecoration(
+                    gradient: AppTheme.backgroundGradient(
+                      primary: Colors.white,
+                      secondary: AppTheme.primaryColor,
+                    ),
+                  )
+                // when the result screen is shown
+                : const BoxDecoration(
+                    color: AppTheme.backgroundColor,
+                    // color: Colors.white,
+                  ),
+            child: Padding(
+              padding: AppSizes.gameScreenMainPadding(context),
+              child: Column(
+                children: [
+                  if (!isGameOver)
+                    const Row(
+                      children: <Widget>[
+                        // left side choices with names
+                        ChoiceA(),
+                        Spacer(),
+                        // right side choices with pictures
+                        ChoiceB()
+                      ],
+                    ),
+                  if (isGameOver)
+                    const Expanded(
+                      child: GameResult(
+                        gameType: GameType.fruit,
+                      ),
+                    ),
+                ],
+              ),
+            )),
+        // floating action button for restarting the game
+        floatingActionButton: FloatingActionButton(
+          onPressed: _restartGame,
+          child: const Icon(Icons.refresh),
+        ),
+      ),
+    );
+  }
+}
